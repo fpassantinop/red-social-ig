f6b453606835dc2d324524359f0f182f3d63ca99
Francis Passantino <francispassantino@Franciss-MacBook-Pro.local>
2021-08-25 17:01:22 -0400

    register admin posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
+"""Posts admin classes."""
+#Django
 from django.contrib import admin
 
-# Register your models here.
+#Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin"""
+    list_display = ('pk', 'user', 'title', 'photo')
+    list_display_links = ('pk', 'user', 'title')
+    list_editable = ('photo',)
+
+    list_filter = (
+                'created',
+                'modified'
+    )
+    
