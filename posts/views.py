46bd78db7522f91662a5d5dc74395f73f2526452
Francis Passantino <francispassantino@Franciss-MacBook-Pro.local>
2021-08-26 11:20:43 -0400

    templates base and nav

--- posts/views.py
+++ posts/views.py
@@ -41,4 +41,4 @@ posts = [
 def list_posts(request):
     """list existing post"""
 
-    return render(request,'feed.html', {'posts': posts})
+    return render(request,'posts/feed.html', {'posts': posts}) 
