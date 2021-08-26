9de44120ab25e3dfdc2b88d4596ad2229abe7777
Francis Passantino <francispassantino@Franciss-MacBook-Pro.local>
2021-08-26 12:37:37 -0400

    sign up user

--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 """User views"""
+#Djando
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+
+#Exceptions
+from django.db.utils import IntegrityError
+
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
 
 def login_view(request):
     """Login views"""
@@ -18,6 +27,35 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
 
     return render(request, 'users/login.html') 
+
+def signup(request):
+    """Sign up view"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password dont match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already taken'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        #add a profile model 
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+
+    return render(request, 'users/signup.html')
 
 @login_required
 def logout_view(request): 
