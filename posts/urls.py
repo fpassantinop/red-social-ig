f4a400dca688a0279a63ad89efe034d295d41223
Francis Passantino <francispassantino@Franciss-MacBook-Pro.local>
2021-08-29 00:07:06 -0400

    add pagination

--- posts/urls.py
+++ posts/urls.py
@@ -17,6 +17,12 @@ urlpatterns = [
         route='posts/new/',
         view=views.create_post,
         name='create'
+    ),
+
+    path(
+        route='posts/<int:pk>/',
+        view=views.PostDetailView.as_view(),
+        name='detail'
     )
 ]
 
